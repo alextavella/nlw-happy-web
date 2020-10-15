68f78a2d321e74350f71cc9a255616d2c0d93ea4
alextavella <alex.tavella-ext@viavarejo.com.br>
2020-10-15 16:26:58 -0300

    Add routes to other pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Landing from "../pages/Landing";
 import OrphanagesMap from "../pages/OrphanagesMap";
+import CreateOrphanage from "../pages/CreateOrphanage";
+import Orphanage from "../pages/Orphanage";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" component={Landing} exact />
       <Route path="/app" component={OrphanagesMap} />
+      <Route path="/orphanages/create" component={CreateOrphanage} />
+      <Route path="/orphanages/:id" component={Orphanage} />
     </Switch>
   );
 };
