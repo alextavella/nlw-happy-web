63bd9cbb4172ae308e8fa863d439d1da2a684a78
alextavella <alex.tavella-ext@viavarejo.com.br>
2020-10-15 16:25:56 -0300

    Include marker on map

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
 
+import "leaflet/dist/leaflet.css";
+
 function App() {
   return (
     <Router>
--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,15 +1,22 @@
+import Leaflet from "leaflet";
 import React from "react";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import mapMarkerImg from "../../images/map-marker.svg";
 import { Container, FloatLink, Footer, Header, MapContainer } from "./styles";
-
-import "leaflet/dist/leaflet.css";
+import { Link } from "react-router-dom";
 
 const mapStyles = {
   width: "100%",
   height: "100%",
 };
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap: React.FC = () => {
   return (
@@ -37,10 +44,24 @@ const OrphanagesMap: React.FC = () => {
           <TileLayer
             url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
           />
+
+          <Marker icon={mapIcon} position={[-23.6096508, -46.5644216]}>
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
+            >
+              Lar das meninas
+              <Link to="/orphanages/1">
+                <FiArrowRight size={20} color="#FFF" />
+              </Link>
+            </Popup>
+          </Marker>
         </Map>
       </MapContainer>
 
-      <FloatLink to="/">
+      <FloatLink to="/orphanages/create">
         <FiPlus size={32} color="#fff" />
       </FloatLink>
     </Container>
